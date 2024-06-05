365251cb6de331f7eede7a487eefd3f6dfec3942
Nick <nikitakarayev@gmail.com>
2024-06-04 17:08:45 -0800

    subfolders added

--- cleanUpDownloads.py
+++ cleanUpDownloads.py
@@ -1,7 +1,9 @@
 import re
 import os
 from datetime import date
+import logging
 
+logger = logging.getLogger(__name__)
 filesTypes = ["txt", "jpg", "jpeg", "pdf", "exe", "rar", "zip", "png"]
 filesDict = {
 			"txt": [],
@@ -15,7 +17,10 @@ filesDict = {
 			"misc": []
 }
 
-DOWNLOADS_DIR = "C:/Users/XIII/Downloads" ## TODO Change to Universal Later
+#DOWNLOADS_DIR = "C:/Users/XIII/Downloads" ## TODO Change to Universal Later
+DOWNLOADS_DIR = "C:/Users/nikit/Downloads"
+FOLDER_TYPES = ["Text Files", "Images", "PDFs", "Executables", "Compressed", "Misc and Unsorted"]
+
 
 ## Get List of all The Files
 def findFileNames():
@@ -31,13 +36,26 @@ def sortFiles(fileList):
 				filesDict[type].append(file)
 			else:
 				filesDict["misc"].append(file)
+
+def createSubFolders(sortedDir):
+	for folder in FOLDER_TYPES:
+		path = os.path.join(sortedDir, folder)
+		os.mkdir(path)
+		logger.info("Folder {} created".format(folder))
+
 				
 def copyFilesToNewDir():
 	newDirName = ("SortedFiles " + str(date.today()))
 	path = os.path.join(DOWNLOADS_DIR, newDirName)
-	os.mkdir(path)
+	#os.mkdir(path)
+	#createSubFolders(path)
+
+	for key in filesDict:
+		if key == "txt":
+			
+
 	##TODO Create Folder for file Types
 	##TODO Sort Files into Correct Folders
 	
-	
+sortFiles(findFileNames())
 copyFilesToNewDir()
